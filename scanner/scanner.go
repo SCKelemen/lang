66d2a05ec7818dbb85de2f5054fa7b9fdd2b8f21
Samuel Kelemen <Samuel@Kelemen.us>
2020-01-25 21:54:15 -0500

    init scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,1 +1,19 @@
 package scanner
+
+// Scanner is responsible for lexing source files
+type Scanner struct {
+	input   string
+	head    int
+	read    int
+	current rune
+}
+
+func New(input string) *Scanner {
+	s := &Scanner{input: input}
+	s.readChar()
+	return s
+}
+
+func (s *Scanner) readChar() {
+
+}
